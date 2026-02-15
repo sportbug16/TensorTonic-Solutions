f53e3fc56bfbc20f0f628bd73869559b308233b1
Nakul Bhachawat <70038936+sportbug16@users.noreply.github.com>
2026-02-15 15:38:04 +0530

    Add solution for q-learning-update

---
+++ q-learning-update/q-learning-update.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def q_learning_update(Q, s, a, r, s_next, alpha, gamma):
+    """
+    Returns: updated Q-table Q_new
+    """
+
+    # Ensure Q is a 2D float NumPy array
+    Q = np.asarray(Q, dtype=float)
+
+    Q_new = Q.copy()
+
+    td_target = r + gamma * np.max(Q[s_next])
+    Q_new[s, a] += alpha * (td_target - Q[s, a])
+
+    return Q_new
+    pass
